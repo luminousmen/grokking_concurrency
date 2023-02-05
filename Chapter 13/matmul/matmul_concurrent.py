bee840100cda99a07a2655139515c2760d31873a
luminousmen <miaplanedo@gmail.com>
2023-02-05 08:49:05 -0800

    Align the main for CH13

--- Chapter 13/matmul/matmul_concurrent.py
+++ Chapter 13/matmul/matmul_concurrent.py
@@ -52,5 +52,8 @@ if __name__ == "__main__":
     cols = 4
     rows = 2
     A = [[random.randint(0, 10) for i in range(cols)] for j in range(rows)]
+    print(f"matrix A: {A}")
     B = [[random.randint(0, 10) for i in range(rows)] for j in range(cols)]
-    parallel_result = matrix_multiply(A, B)
+    print(f"matrix B: {B}")
+    C = matrix_multiply(A, B)
+    print(f"matrix C: {C}")
