602ba51183e5a3a2b32f3760212a73b20d5bb6b7
luminousmen <miaplanedo@gmail.com>
2023-05-05 16:06:08 -0700

    Formatting the code for the final annotations

--- Chapter 11/event_loop.py
+++ Chapter 11/event_loop.py
@@ -11,6 +11,7 @@ import typing as T
 
 
 class Event:
+    """Represents an event that can be executed by the event loop"""
     def __init__(self, name: str, action: T.Callable[..., None],
                  next_event: T.Optional[Event] = None) -> None:
         self.name = name
@@ -18,24 +19,30 @@ class Event:
         self._next_event = next_event
 
     def execute_action(self) -> None:
+        """Execute the action of the event and register the next event if
+        it exists."""
         self._action(self)
         if self._next_event:
             event_loop.register_event(self._next_event)
 
 
 class EventLoop:
-    """ Maintains a deque of Events and executes them. """
+    """ A class representing an event loop that maintains a deque of Events
+    and executes them. """
 
     def __init__(self) -> None:
         # internal event queue
         self._events: deque[Event] = deque()
 
     def register_event(self, event: Event) -> None:
+        """Registers an event with the event loop"""
         self._events.append(event)
 
-    def run_forever(self) -> T.NoReturn:
+    def run_forever(self) -> None:
+        """Execute the actions of the events in the deque in a loop until
+        the program is terminated."""
         print(f"Queue running with {len(self._events)} events")
-        while True:   # busy-waiting
+        while True:  # busy-waiting
             # execute the action of the next event
             try:
                 event = self._events.popleft()
@@ -45,11 +52,15 @@ class EventLoop:
 
 
 def knock(event: Event) -> None:
+    """A callback function that prints the name of the event and sleeps
+    for one second."""
     print(event.name)
     sleep(1)
 
 
 def who(event: Event) -> None:
+    """A callback function that prints the name of the event and sleeps
+    for one second."""
     print(event.name)
     sleep(1)
 
--- Chapter 11/pizza_reactor.py
+++ Chapter 11/pizza_reactor.py
@@ -2,17 +2,17 @@
 
 """Implementation of event-based concurrency using Reactor pattern"""
 
+import typing as T
 from selectors import DefaultSelector, EVENT_READ, EVENT_WRITE
 from socket import socket, create_server
-import typing as T
 
 Data = bytes
-Action = T.Union[T.Callable[[socket], None],
-               T.Tuple[T.Callable[[socket, Data], None], str]]
+Action = T.Union[T.Callable[[socket], None], T.Tuple[
+    T.Callable[[socket, Data], None], str]]
 
 # the maximum amount of data to be received at once
 BUFFER_SIZE = 1024
-ADDRESS = ("127.0.0.1", 12345)   # address and port of the host machine
+ADDRESS = ("127.0.0.1", 12345)  # address and port of the host machine
 
 
 class EventLoop:
@@ -25,47 +25,63 @@ class EventLoop:
         # roughly epoll|kqueue|devpoll > poll > select
         self.event_notifier = DefaultSelector()
 
-    def register_event(self, source: socket, event: int, action: Action) -> None:
+    def register_event(self, source: socket, event: int,
+                       action: Action) -> None:
+        """Registers an event to the event notifier with the given source,
+        event, and action. If the event already exists, modifies the action
+        of the event."""
         try:
             self.event_notifier.register(source, event, action)
         except KeyError:  # already exists so modify
             self.event_notifier.modify(source, event, action)
 
-    def run_forever(self) -> T.NoReturn:
+    def unregister_event(self, source: socket) -> None:
+        """Unregisters the event with the given source from the event
+        notifier."""
+        self.event_notifier.unregister(source)
+
+    def run_forever(self) -> None:
+        """Runs the event loop indefinitely, waiting for registered events
+        and executing their associated actions when they occur"""
         while True:
-            # .select() blocks until there are sockets ready for I/O.
+            # select() blocks until there are sockets ready for I/O.
             events = self.event_notifier.select()
             for (source, _, _, action), event in events:
                 if event & EVENT_READ:
                     action(source)
                 elif event & EVENT_WRITE:
-                    action, msg = action   # separate callable and data
+                    action, msg = action  # separate callable and data
                     action(source, msg)
                 else:
-                    raise RuntimeError
+                    raise RuntimeError("No such event")
 
 
 class Server:
+    """A simple TCP server that uses event-based concurrency with the
+    reactor pattern."""
+
     def __init__(self, event_loop: EventLoop) -> None:
         self.event_loop = event_loop
         try:
             print(f"Starting up at: {ADDRESS}")
             # On POSIX platforms the SO_REUSEADDR socket option is set
             self.server_socket = create_server(ADDRESS)
-            print("Listen for incoming connections")
-            # Let's start listening mode for this socket
-            self.server_socket.listen()
-            print("Waiting for a connection")
+            # set socket to non-blocking mode
             self.server_socket.setblocking(False)
+            # on server side let's start listening mode for this socket
+            self.server_socket.listen()
         except OSError:
             self.server_socket.close()
             print("\nServer stopped.")
 
-    def _on_accept(self, conn: socket) -> None:
-        # accepting the incoming connection, non-blocking
-        # conn = is a new socket object usable to send and receive data on
-        # the connection on the other end of connection
-        conn, client_address = self.server_socket.accept()
+    def _on_accept(self, _: socket) -> None:
+        """Callback that is called when a new incoming client connection
+        is accepted by the server socket. It registers the connected socket
+        for read events."""
+        try:
+            conn, client_address = self.server_socket.accept()
+        except BlockingIOError:
+            return
         conn.setblocking(False)
         print(f"Connected to {client_address}")
         # future calls to the self.event_notifier.select() will be notified
@@ -73,27 +89,37 @@ class Server:
         self.event_loop.register_event(conn, EVENT_READ, self._on_read)
 
     def _on_read(self, conn: socket) -> None:
-        print(f"Message received from {conn.getpeername()}")
-        data = conn.recv(BUFFER_SIZE)
-        if data:
-            message = data.decode()
-            self.event_loop.register_event(
-                conn, EVENT_WRITE, (self._on_write, message))
-        else:
-            self.event_loop.event_notifier.unregister(conn)
+        """Callback that is called when the connected socket has incoming
+        data to be read. It reads data from the socket, handles the
+        received message and registers the socket for write events if
+        required."""
+        try:
+            data = conn.recv(BUFFER_SIZE)
+        except BlockingIOError:
+            return
+        if not data:
+            self.event_loop.unregister_event(conn)
             print(f"Connection with {conn.getpeername()} has been closed")
             conn.close()
+            return
+        message = data.decode().strip()
+        self.event_loop.register_event(conn, EVENT_WRITE,
+                                       (self._on_write, message))
 
     def _on_write(self, conn: socket, message: bytes) -> None:
-        print(f"Sending a message to {conn.getpeername()}")
+        """Callback that is called when the connected socket is ready to
+        write data. It writes the message to the socket and registers the
+        socket for read events."""
         try:
             order = int(message)
             response = f"Thank you for ordering {order} pizzas!\n"
         except ValueError:
             response = "Wrong number of pizzas, please try again\n"
         print(f"Sending message to {conn.getpeername()}")
-        # send a response
-        conn.send(response.encode())
+        try:
+            conn.send(response.encode())
+        except BlockingIOError:
+            return
         self.event_loop.register_event(conn, EVENT_READ, self._on_read)
 
     def start(self) -> None:
@@ -101,6 +127,7 @@ class Server:
         # Once register is done future calls to the self.event_notifier.select()
         # will be notified whether server socket has any pending accept events
         # from the clients
+        print("Server listening for incoming connections")
         self.event_loop.register_event(
             self.server_socket, EVENT_READ, self._on_accept)
 
